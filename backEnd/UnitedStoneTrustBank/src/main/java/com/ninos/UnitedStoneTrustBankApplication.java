3c225d3f01930d504295663465c139b466ead99a
NinoosMoshi <nalkaton@gmail.com>
2025-12-28 21:38:15 -0600

    create spring boot

---
+++ backEnd/UnitedStoneTrustBank/src/main/java/com/ninos/UnitedStoneTrustBankApplication.java
@@ -0,0 +1,13 @@
+package com.ninos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitedStoneTrustBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitedStoneTrustBankApplication.class, args);
+	}
+
+}
